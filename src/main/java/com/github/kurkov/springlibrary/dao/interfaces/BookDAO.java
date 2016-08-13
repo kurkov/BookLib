494c2a3cb0284d8eb411dc6fc6151e8facad8da1
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2016-08-13 19:46:09 +0300

    Initial commit

---
+++ src/main/java/com/github/kurkov/springlibrary/dao/interfaces/BookDAO.java
@@ -0,0 +1,19 @@
+package com.github.kurkov.springlibrary.dao.interfaces;
+
+import com.github.kurkov.springlibrary.entities.Author;
+import com.github.kurkov.springlibrary.entities.Book;
+import com.github.kurkov.springlibrary.entities.Genre;
+
+import java.util.List;
+
+/**
+ * @author Aleksey Kurkov. Created on 13.08.2016
+ */
+public interface BookDAO {
+
+    List<Book> getBooks();
+    List<Book> getBooks(Author author);
+    List<Book> getBooks(String bookName);
+    List<Book> getBooks(Genre genre);
+    List<Book> getBooks(Character letter);
+}
