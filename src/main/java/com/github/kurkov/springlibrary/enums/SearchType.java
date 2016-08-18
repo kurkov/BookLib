823b8b2e4240a3e7af703aa805d863e4f1403051
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2016-08-18 22:10:45 +0300

    implemented search types with locale

---
+++ src/main/java/com/github/kurkov/springlibrary/enums/SearchType.java
@@ -0,0 +1,10 @@
+package com.github.kurkov.springlibrary.enums;
+
+/**
+ * @author Aleksey Kurkov. Created on 18.08.2016
+ */
+public enum SearchType {
+
+    AUTHOR,
+    TITLE
+}
--- src/main/java/com/github/kurkov/springlibrary/objects/Utils.java
+++ src/main/java/com/github/kurkov/springlibrary/objects/Utils.java
@@ -1,6 +1,13 @@
 package com.github.kurkov.springlibrary.objects;
 
+import com.github.kurkov.springlibrary.enums.SearchType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Component;
+
+import javax.faces.context.FacesContext;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Aleksey Kurkov. Created on 13.08.2016
@@ -8,10 +15,32 @@ import org.springframework.stereotype.Component;
 @Component
 public class Utils {
 
+    private Map<String, SearchType> searchTypeList = new HashMap<>();
+
+    private SearchType selectedSearchType = SearchType.TITLE; // default value
+
+    @Autowired
+    private MessageSource msg;
+
     private Character[] letters = new Character[]{'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я'};
 
     public Character[] getLetters() {
         return letters;
+    }
+
+    public Map<String, SearchType> getSearchTypeList() {
+        searchTypeList.clear();
+        searchTypeList.put(msg.getMessage("author_name", null, FacesContext.getCurrentInstance().getViewRoot().getLocale()), SearchType.AUTHOR);
+        searchTypeList.put(msg.getMessage("book_name", null, FacesContext.getCurrentInstance().getViewRoot().getLocale()), SearchType.TITLE);
+        return searchTypeList;
+    }
+
+    public SearchType getSelectedSearchType() {
+        return selectedSearchType;
+    }
+
+    public void setSearchTypeList(Map<String, SearchType> searchTypeList) {
+        this.searchTypeList = searchTypeList;
     }
 
 }
