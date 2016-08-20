c3fd6365c4b2576b36196eb7cc9e491d5ed731e6
Aleksey Kurkov <aleksey.kurkov@gmail.com>
2016-08-20 16:53:48 +0300

    implemented book search by letter

--- src/main/java/com/github/kurkov/springlibrary/objects/LibraryFacade.java
+++ src/main/java/com/github/kurkov/springlibrary/objects/LibraryFacade.java
@@ -3,6 +3,7 @@ package com.github.kurkov.springlibrary.objects;
 import com.github.kurkov.springlibrary.dao.interfaces.BookDAO;
 import com.github.kurkov.springlibrary.entities.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,21 +12,25 @@ import java.util.List;
  * @author Aleksey Kurkov. Created on 19.08.2016
  */
 @Component
+@Scope("singleton")
 public class LibraryFacade {
 
-
+    @Autowired
     private BookDAO bookDAO;
 
     @Autowired
-    public void setBookDAO(BookDAO bookDAO) {
-        this.bookDAO = bookDAO;
-        books = bookDAO.getBooks();
-    }
+    private SearchCriteria searchCriteria;
 
     private List<Book> books;
 
-
     public List<Book> getBooks() {
+        if (books == null) {
+            books = bookDAO.getBooks();
+        }
         return books;
+    }
+
+    public void searchBooksByLetter() {
+        books = bookDAO.getBooks(searchCriteria.getLetter());
     }
 }
---
+++ src/main/java/com/github/kurkov/springlibrary/objects/SearchCriteria.java
@@ -0,0 +1,56 @@
+package com.github.kurkov.springlibrary.objects;
+
+import com.github.kurkov.springlibrary.entities.Genre;
+import com.github.kurkov.springlibrary.enums.SearchType;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * @author Aleksey Kurkov. Created on 20.08.2016
+ */
+@Component
+@Scope("singleton")
+public class SearchCriteria implements Serializable {
+
+    private String text;
+
+    private SearchType searchType;
+
+    private Character letter;
+
+    private Genre genre;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public SearchType getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(SearchType searchType) {
+        this.searchType = searchType;
+    }
+
+    public Character getLetter() {
+        return letter;
+    }
+
+    public void setLetter(Character letter) {
+        this.letter = letter;
+    }
+
+    public Genre getGenre() {
+        return genre;
+    }
+
+    public void setGenre(Genre genre) {
+        this.genre = genre;
+    }
+}
